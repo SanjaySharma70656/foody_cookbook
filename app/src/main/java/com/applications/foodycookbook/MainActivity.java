ed917af52f86f7bc980e482d4a0400828ba93f31
ClasherLucifer <sanjay.sharma.70656@gmail.com>
2021-04-15 02:21:59 +0530

    Commit after try harding

--- app/src/main/java/com/applications/foodycookbook/MainActivity.java
+++ app/src/main/java/com/applications/foodycookbook/MainActivity.java
@@ -1,16 +1,45 @@
 package com.applications.foodycookbook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import android.database.sqlite.*;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //test
+        //Hooks
+        bottomNavigationView=findViewById(R.id.bottomNavigationView);
+        //
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId())
+                {
+                    case R.id.itemSearch:
+                        Intent intent=new Intent(getApplicationContext(),SearchActivity.class);
+                        startActivity(intent);
+                        break;
+                }
+                return false;
+            }
+        });
+
+
+
     }
 }
---
+++ app/src/main/java/com/applications/foodycookbook/SearchActivity.java
@@ -0,0 +1,114 @@
+package com.applications.foodycookbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.JsonReader;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.*;
+import java.lang.Object;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class SearchActivity extends AppCompatActivity {
+
+    ImageView imgSearch;
+    EditText etSearch;
+    Button searchResult;
+
+    String resultString;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        //Hooks
+        imgSearch=findViewById(R.id.imgSearch);
+        etSearch=findViewById(R.id.etSearch);
+        searchResult=findViewById(R.id.searchResult);
+
+
+        //Variables
+        String searchTerm;
+
+
+        //url : www.themealdb.com/api/json/v1/1/
+        searchTerm=etSearch.getText().toString();
+
+        imgSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //
+                AsyncTask.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        //
+                        try
+                        {
+                            URL url=new URL("https://www.themealdb.com/api/json/v1/1/search.php?s="+searchTerm);
+                            HttpURLConnection connection=(HttpURLConnection)url.openConnection();
+                            if (connection.getResponseCode() == 200)
+                            {
+                                // Success
+                                System.out.print("Success!");
+                                // Further processing here
+                                InputStreamReader responseReader=new InputStreamReader(connection.getInputStream());
+                                JsonReader jsonReader=new JsonReader(responseReader);
+                                jsonReader.beginObject();  //Process the json object
+
+                                while(jsonReader.hasNext())
+                                {
+                                    String key=jsonReader.nextName();
+                                    if(key.equals("strMeal"))
+                                    {
+                                        resultString=jsonReader.nextString();
+                                        searchResult.setText(resultString);
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        jsonReader.skipValue();
+                                    }
+                                }
+
+                            } else
+                            {
+                                // Error handling code goes here
+                            }
+                        }
+                        catch(Exception e){
+                            System.out.print("HTTP error");
+                        }
+                    }
+                });
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
